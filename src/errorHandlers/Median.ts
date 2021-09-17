a99b2ff96b5cfa5f7c73244fe0f71b4cbef8a8a5
SchreinerCarin <mail@carin-schreiner.de>
2021-09-17 22:21:58 +0200

    fixed Median.ts

--- src/errorHandlers/Median.ts
+++ src/errorHandlers/Median.ts
@@ -2,11 +2,11 @@ import {getValueByIndexFunction} from "../../index";
 
 export function getMedian (length: number, getY: getValueByIndexFunction, index: number): number {
     let middle = (length / 2);
-    if(length % 2 === 0){
-        let first = getY( Math.floor(middle) - 1);
-        let second = getY( Math.ceil(middle) - 1);
+    if(length % 2 == 0){
+        let first = getY( middle - 1);
+        let second = getY( middle );
         return (first + second) / 2;
     } else {
-        return getY(middle - 1);
+        return getY(Math.floor(middle) - 1);
     }
 }
